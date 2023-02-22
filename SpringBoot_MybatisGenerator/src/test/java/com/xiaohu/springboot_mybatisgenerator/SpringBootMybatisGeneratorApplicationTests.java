525763bd34087495f03ee740271af8f9bf347cf0
zhangxiaohu <zhangxiaohu@nti56.com>
2023-02-22 17:54:44 +0800

    整合mybatisgenerator

---
+++ SpringBoot_MybatisGenerator/src/test/java/com/xiaohu/springboot_mybatisgenerator/SpringBootMybatisGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaohu.springboot_mybatisgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMybatisGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
