21863650ed8089c4a3ae87584e0372b4b7cf86dd
zhangxiaohu <zhangxiaohu@nti56.com>
2023-02-24 12:40:16 +0800

    整合Quartz,使用mysql存储指令

---
+++ SpringBoot_MYSQL/src/test/java/com/xiaohu/springboot_mysql/SpringBootMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaohu.springboot_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
